45c166eff0be408ffbf77d55d0c0957a585fd129
Abirami Jothi <abiramijothi15@gmail.com>
2023-07-17 20:34:10 +0530

    first commit

---
+++ add.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// define the variables we want to add
+	var number1, number2, number3 int
+
+	// initializing the variables
+	number1 = 99
+	number2 = 81
+
+	// adding the numbers
+	number3 = number1 + number2
+
+	// printing the results
+	fmt.Println("The addition of ", number1, " and ", number2, " is \n ", number3, "\n(Addition of two integers within the function)")
+}
